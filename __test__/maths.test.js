4a73e62036723f49c57cc0c17bc8affcb95e3b60
jose.montoya2 <jose.montoya2.next@bbva.com>
2019-06-14 14:30:37 -0500

    funciones matematicas testeadas | primer ejemplo de pruebas unitarias

---
+++ __test__/maths.test.js
@@ -0,0 +1,16 @@
+import  { sumNums, subsNums, mutiplyNums, divideNums }  from '../maths.js'
+
+describe('Math Operations', () => {
+    test('Test for sumNums', () => {
+        expect(sumNums(1,0)).toBe(1)
+    })
+    test('Test for subsNums', () => {
+        expect(subsNums(4,2)).toBe(2)
+    })
+    test('Test for mutiplyNums', () => {
+        expect(mutiplyNums(1,3)).toBe(3)
+    })
+    test('Test for divideNums', () => {
+        expect(divideNums(8,2)).toBe(4)
+    }) 
+})
---
+++ maths.js
@@ -0,0 +1,15 @@
+export const sumNums = (a, b) => {
+    return a + b;
+}
+
+export const subsNums = (a, b) => {
+    return a - b;
+}
+
+export const mutiplyNums = (a, b) => {
+    return a * b;
+}
+
+export const divideNums = (a, b) => {
+    return a / b;
+}
